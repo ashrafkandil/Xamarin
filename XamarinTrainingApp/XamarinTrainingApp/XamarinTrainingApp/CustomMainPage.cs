a0a38dc6d32530d27aa3344330b6dfbaa06b0c3f
ashrafkandil <ashraf.kandil@gmail.com>
2017-01-05 12:29:36 -0500

    training code 1

---
+++ XamarinTrainingApp/XamarinTrainingApp/XamarinTrainingApp/CustomMainPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamarinTrainingApp
+{
+    public class CustomMainPage : ContentPage
+    {
+        public CustomMainPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello from my custom page" }
+                }
+            };
+        }
+    }
+}
